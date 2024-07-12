5e0bec28ece234ff519a0b92654a5b6bcc114431
RaymondHornDev <132532625+RaymondHornDev@users.noreply.github.com>
2024-07-11 23:55:55 -0400

    Update

--- DoubleLinkedList/Program.cs
+++ DoubleLinkedList/Program.cs
@@ -10,24 +10,12 @@ namespace Main{
 
             LinkedList linkedList = new LinkedList();
             FillList(ref values, ref linkedList);
-            WalkList(linkedList);
+            linkedList.WalkList(linkedList);
         }
 
         static void FillList(ref int[] list, ref LinkedList passedList){
             for (int iter = 0; iter < list.Length; iter++) { 
                 passedList.Insert(list[iter]);
-            }
-        }
-
-        static void WalkList(LinkedList passedList){
-            Node node = passedList.head;
-            RecursiveWalk(node);
-        }
-
-        static void RecursiveWalk(Node passedNode){
-            WriteLine(passedNode.value);
-            if (passedNode.next != null) { 
-                RecursiveWalk(passedNode.next);
             }
         }
     }
@@ -68,6 +56,18 @@ namespace Main{
             }
             else{
                 node.next = new Node(value, node);
+            }
+        }
+
+        public void WalkList(LinkedList passedList){
+            Node node = passedList.head;
+            RecursiveWalk(node);
+        }
+
+        void RecursiveWalk(Node passedNode){
+            WriteLine(passedNode.value);
+            if (passedNode.next != null) { 
+                RecursiveWalk(passedNode.next);
             }
         }
     }
