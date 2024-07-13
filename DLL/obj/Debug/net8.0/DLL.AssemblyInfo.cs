7548579715c80157c16fc16e3daae9b3d85618fe
RaymondHornDev <132532625+RaymondHornDev@users.noreply.github.com>
2024-07-12 23:45:36 -0400

    Update

--- DLL/obj/Debug/net8.0/DLL.AssemblyInfo.cs
+++ DLL/obj/Debug/net8.0/DLL.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DLL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+51bb2ea036036e1e1b5a62f5e15601ee9a04e23f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+74fd553ba16ca6f2eee81e4cc4bf3d2254493754")]
 [assembly: System.Reflection.AssemblyProductAttribute("DLL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DLL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ Deque/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using static System.Console;
+
+namespace Main
+{
+    public class Program
+    {
+        static void Main(string[] args) {
+            Deque dq = new Deque();
+            int[] myArray = { 4, 2, 3, 5, 8, 6, 7 };
+
+            Program.RecursiveFill(myArray, 0, dq);
+            Program.RecursiveEmpty(dq);
+        }
+
+        static void RecursiveFill(int[] list, int value, Deque deque)
+        {
+            if(value < list.Length)
+            {
+                deque.Push(list[value]);
+                RecursiveFill(list, value + 1, deque);
+            }
+        }
+
+        static void RecursiveEmpty(Deque deque){
+            if(deque.HasNodes == true){
+                WriteLine(deque.Pop().value);
+                RecursiveEmpty(deque);
+            }
+        }
+    }
+
+    class Node
+    {
+        public int value;
+        public Node next = null;
+        public Node prev = null;
+
+        public Node() { }
+        public Node(int value)
+        {
+             this.value = value;
+        }
+    }
+
+    class Deque
+    {
+        public bool HasNodes = false;
+        Node head = null;
+        Node tail = null;
+
+        public void Push(int value)
+        {
+            Node n = new Node(value);
+            Node localHead = head;
+
+            if (head == null)
+            {
+                tail = n;
+                head = n;
+                HasNodes = true;
+            }
+            else
+            {
+                n.next = head;
+                head.prev = n;
+                head = n;
+            }
+        }
+
+         public Node Pop()
+         {
+            Node return_node = tail;
+            tail = return_node.prev;
+            if(tail == null){
+                HasNodes = false;
+            }
+            return return_node;
+        }
+    }
+}
