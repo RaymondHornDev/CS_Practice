cb1a352ffe0b2dfe240ad37cdb4aea73354b0afe
RaymondHornDev <132532625+RaymondHornDev@users.noreply.github.com>
2024-07-16 21:38:57 -0400

    update

--- DLL/obj/Debug/net8.0/DLL.AssemblyInfo.cs
+++ DLL/obj/Debug/net8.0/DLL.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DLL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dcafacc93f0219c60cfacb28e66597a7b6f358a1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+83fd87e08b96c4456a68712fe9497ca26f2e23b1")]
 [assembly: System.Reflection.AssemblyProductAttribute("DLL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DLL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ Every_Other_Letter/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program{
+    static void Main(string[] args) {
+        string nString = "hello";
+        Console.WriteLine(CapEOL(nString, 1));
+    }
+
+    static string CapEOL(string _string, int val){
+        string ret_string = "";
+        if(val - 1 < _string.Length){
+            if(val % 2 == 0){
+                return ret_string += _string.Substring(val - 1, 1).ToUpper() + CapEOL(_string, val + 1);
+            }
+            else{
+                return ret_string += _string.Substring(val - 1, 1).ToLower() + CapEOL(_string, val + 1);
+            }
+        }
+        return ret_string;
+    }
+}
