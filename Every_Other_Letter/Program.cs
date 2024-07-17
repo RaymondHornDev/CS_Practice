5c839f4ea0f9ff0c143b83c3692c6d96ec0f6969
RaymondHornDev <132532625+RaymondHornDev@users.noreply.github.com>
2024-07-16 21:43:13 -0400

    Update

--- Every_Other_Letter/Program.cs
+++ Every_Other_Letter/Program.cs
@@ -3,18 +3,19 @@
 class Program{
     static void Main(string[] args) {
         string nString = "hello";
-        Console.WriteLine(CapEOL(nString, 1));
+        Console.WriteLine(CapEOL(nString, 0));
     }
 
     static string CapEOL(string _string, int val){
         string ret_string = "";
-        if(val - 1 < _string.Length){
-            if(val % 2 == 0){
-                return ret_string += _string.Substring(val - 1, 1).ToUpper() + CapEOL(_string, val + 1);
+        if(val < _string.Length){
+            if((val + 1) % 2 == 0){
+                ret_string += _string.Substring(val, 1).ToUpper();
             }
             else{
-                return ret_string += _string.Substring(val - 1, 1).ToLower() + CapEOL(_string, val + 1);
+                ret_string += _string.Substring(val, 1).ToLower();
             }
+            return ret_string += CapEOL(_string, val + 1);
         }
         return ret_string;
     }
