de5f819e1c05a8295929c6a0a0ee70a68dbea19f
RaymondHornDev <132532625+RaymondHornDev@users.noreply.github.com>
2024-07-16 22:47:53 -0400

    Update

---
+++ AutoCompleteDivTags/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class Program{
+    static void Main(string[] args) {
+        string locHtml = "<Div new line DIV>";
+
+        Console.WriteLine(AutoComplete(locHtml, 0));
+
+    }
+
+    static string AutoComplete(string html, int val){
+        string retString = "";
+        if(val < html.Length){
+            if(html.Substring(val, 1) == "<" && html.Substring(val + 4, 1) != ">"){
+                if(html.Substring(val + 1, 1).ToLower() == "d" && html.Substring(val + 3, 1).ToLower() == "v"){
+                    retString += "<div> ";
+                    val += 4;
+                }
+            }
+            else if (html.Substring(val, 1) != "<" && html.Substring(val + 4, 1) == ">"){
+                if (html.Substring(val + 1, 1).ToLower() == "d" && html.Substring(val + 3, 1).ToLower() == "v")
+                {
+                    retString += " <div>";
+                    val += 4;
+                }
+            }
+            else{
+                retString += html.Substring(val, 1);
+            }
+            return retString + AutoComplete(html, val + 1);
+        }
+        return retString;
+    }
+}
